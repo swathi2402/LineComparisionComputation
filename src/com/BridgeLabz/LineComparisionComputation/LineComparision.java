be0db83ff5a77ef64a9ef6203e929b827dc955c0
Swathi <swathi.h@perfios.com>
2021-08-20 16:46:09 +0530

    Calculating length of the line

--- src/com/BridgeLabz/LineComparisionComputation/LineComparision.java
+++ src/com/BridgeLabz/LineComparisionComputation/LineComparision.java
@@ -1,7 +1,17 @@
 package com.BridgeLabz.LineComparisionComputation;
 
+import java.util.Scanner;
+
 public class LineComparision {
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Line Comparison Computation Program *****");
+		System.out.println("Enter line co-ordinates:");
+		Scanner scanIn = new Scanner(System.in);
+		double x1 = scanIn.nextInt();
+		double y1 = scanIn.nextInt();
+		double x2 = scanIn.nextInt();
+		double y2 = scanIn.nextInt();
+		double distance =  Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		System.out.println("Length of the line:" + distance);
 	}
 }
