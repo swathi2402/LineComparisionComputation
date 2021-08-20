4756db7b69fe77401333e0813aa6b9951fb3dce1
Swathi <swathi.h@perfios.com>
2021-08-20 17:10:04 +0530

    Using Java equals method to check equality of 2 lines

--- src/com/BridgeLabz/LineComparisionComputation/LineComparision.java
+++ src/com/BridgeLabz/LineComparisionComputation/LineComparision.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class LineComparision {
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Line Comparison Computation Program *****");
-		System.out.println("Enter line co-ordinates:");
+		System.out.println("Enter co-ordinates of first line:");
 		Scanner scanIn = new Scanner(System.in);
 		double x1 = scanIn.nextInt();
 		double y1 = scanIn.nextInt();
 		double x2 = scanIn.nextInt();
 		double y2 = scanIn.nextInt();
-		double distance =  Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-		System.out.println("Length of the line:" + distance);
+		System.out.println("Enter co-ordinates of second line:");
+		double a = scanIn.nextInt();
+		double b = scanIn.nextInt();
+		double c = scanIn.nextInt();
+		double d = scanIn.nextInt();
+		String distance1 = String.valueOf(Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)));
+		String distance2 = String.valueOf(Math.sqrt((d - b) * (d - b) + (c - a) * (c - a)));
+		if (distance1.equals(distance2)) {
+			System.out.println("Two lines are of same length");
+		} else {
+			System.out.println("Two lines are of different length");
+		}
 	}
 }
