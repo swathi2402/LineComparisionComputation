b4c6558adbd8f21eec6dc86b9f9c3eff4fd9ae5a
Swathi <swathi.h@perfios.com>
2021-08-20 15:40:23 +0530

    Initial Commit

---
+++ src/com/BridgeLabz/LineComparisionComputation/LineComparision.java
@@ -0,0 +1,7 @@
+package com.BridgeLabz.LineComparisionComputation;
+
+public class LineComparision {
+	public static void main(String[] args) {
+		System.out.println("***** Welcome to Line Comparison Computation Program *****");
+	}
+}
