e2a690e33d966bdd165e32d3746c5ca09f8ea4ec
Swathi <swathi.h@perfios.com>
2021-08-23 08:15:36 +0530

    ADDED: Logic to find equality of the two lines

--- src/com/BridgeLabz/LineComparisionComputation/LineComparision.java
+++ src/com/BridgeLabz/LineComparisionComputation/LineComparision.java
@@ -16,25 +16,14 @@ public class LineComparision {
 		double b = scanIn.nextInt();
 		double c = scanIn.nextInt();
 		double d = scanIn.nextInt();
-		
+
 		String distance1 = String.valueOf(Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)));
 		String distance2 = String.valueOf(Math.sqrt((d - b) * (d - b) + (c - a) * (c - a)));
-		
+
 		if (distance1.equals(distance2)) {
 			System.out.println("Two lines are of same length");
 		} else {
 			System.out.println("Two lines are of different length");
-		}
-		
-		int result = distance1.compareTo(distance2);
-		if (result == 0) {
-			System.out.println("Two lines are of same length");
-		} 
-		else if (result < 0) {
-			System.out.println("First line is lesser than second");
-		} 
-		else if (result > 0) {
-			System.out.println("First line is greater than second");
 		}
 	}
 }
