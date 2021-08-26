9ed025e9e31d994b490e60331b0b8b165ab0bd98
Swathi <swathi.h@perfios.com>
2021-08-26 16:22:34 +0530

    REFACTORED: Using classes, interface and OOPs concept

---
+++ src/com/BridgeLabz/linecomparisionoop/LengthComparatorIF.java
@@ -0,0 +1,8 @@
+package com.BridgeLabz.linecomparisionoop;
+
+public interface LengthComparatorIF {
+	
+	public void isLinesEqual(String distance1, String distance2);
+	public void lineToCompare(String distance1, String distance2);
+	
+}
